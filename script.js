49754ce92d1096bac630830033e9b69162706d54
Oleg <rich007rich007rich@mail.ru>
2017-06-19 22:46:32 +0300

    commit

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ $('.block').offset({
 
 $('.iphone').scroll(function(){
 	x = $('.iphone').scrollTop();
-	console.log(x);
 })
 
 var n = 0;
@@ -16,4 +15,16 @@ var timer = setInterval(function(){
 	if(n >= 9) {
 		clearInterval(timer);
 	}
-}, 2000)
+	if(n == 6 && $('.iphone').scrollTop() < 85) {
+		$('.iphone').animate({scrollTop: 85}, 500);
+	}
+	if(n == 7 && $('.iphone').scrollTop() < 170) {
+		$('.iphone').animate({scrollTop: 170}, 500);
+	}
+	if(n == 8 && $('.iphone').scrollTop() < 255) {
+		$('.iphone').animate({scrollTop: 255}, 500);
+	}
+	if(n == 9 && $('.iphone').scrollTop() < 340) {
+		$('.iphone').animate({scrollTop: 340}, 500);
+	}
+}, 1000)
